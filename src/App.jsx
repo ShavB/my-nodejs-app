70551dafabd895e99b55d0b5aa550d1f0e2ed7e2
ShyamBisht <bishtshyam135@gmail.com>
2025-05-09 16:07:31 +0100

    added header for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import "./App.css";
 
 function App() {
+  const headerLink = [
+    {
+      id: 1,
+      label: "Home",
+    },
+    {
+      id: 2,
+      label: "Projects",
+    },
+    {
+      id: 3,
+      label: "About",
+    },
+    {
+      id: 4,
+      label: "Contact",
+    },
+  ];
   return (
     <>
+      <div className="header">
+        <div>logo</div>
+        {headerLink.map((tab) => (
+          <a key={tab.id} href="#">
+            {tab.label}
+          </a>
+        ))}
+      </div>
       <div>
         <h1>This is vite react app</h1>
         <table>
