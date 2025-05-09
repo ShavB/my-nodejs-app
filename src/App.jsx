8bc84ec04b55f35b8d0a1478cdbfb47d233f27f1
ShyamBisht <bishtshyam135@gmail.com>
2025-05-09 16:02:36 +0100

    added vite frontend app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>This is vite react app</h1>
+        <table>
+          <thead>head</thead>
+          <tbody>
+            <tr>row 1</tr>
+            <tr>row 2</tr>
+            <tr>row 3</tr>
+            <tr>row 4</tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
